32a1f6c2508cf1a2c21b58f4358fb53f588c6d6a
Cassandra <cassandralelei013@gmail.com>
2023-08-16 07:49:02 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a funtion write_file."""
+"""Defines a funtion append_write."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """A function that appends a string to a text file.
 
     Args:
