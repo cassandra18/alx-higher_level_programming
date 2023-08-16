c8905387e63b2b6edcf8d049ed97d423e37048cc
Cassandra <cassandralelei013@gmail.com>
2023-08-16 06:48:38 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Defines a function read_file."""
 
 
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except:
-        pass
+    """Read and print the content of a text file to stdout.
+
+    Args:
+        filename: The path of the text file to be read.
+                  Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
