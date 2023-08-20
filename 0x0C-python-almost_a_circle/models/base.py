56db04ff20ad23b00e5257a84902a32e9e0ed8d1
cassandra18 <cassandralelei@gmail.com>
2023-08-20 04:35:00 -0700

    first commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""A moduke that defines a class-Base."""
+
+
+import json
+
+
+class Base:
+    """
+    A class that defines a method used to reeturn the an instance
+    id.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        A method that initializes the class, Base.
+
+        Args:
+            id = public instance attribute. Default value is None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+        with open(filename, "w") as f:
+            f.write(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+                list_dicts = json.loads(json_string)
+                list_instances = [cls.create(**dict_obj) for dict_obj in list_dicts]
+                return list_instances
+        except FileNotFoundError:
+            return []
